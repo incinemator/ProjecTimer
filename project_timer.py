e43ecb3320a5cb00ce461cd747e94734d3fd7446
incinemator <giannakzorz@gmail.com>
2024-04-04 17:32:51 +0200

    start_timer function

--- project_timer.py
+++ project_timer.py
@@ -5,3 +5,9 @@ import time
 start_time = None
 elapsed_time = 0
 is_paused = False
+
+# Start the timer
+def start_timer():
+    global start_time, is_paused
+    start_time = time.time()
+    is_paused = False

23bbfc5e89fa52fd1ee31c180257efc9a3af9fe3
incinemator <giannakzorz@gmail.com>
2024-04-13 09:28:21 +0200

    Update format time

--- project_timer.py
+++ project_timer.py
@@ -83,10 +83,10 @@ class PT_ProjectTimer(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        timer = 
+        timer = context.scene.timer
 
         # Display elapsed time
-        layout.label(text="Running Time: {.2f} seconds" .format(context.scene.timer.format_time(context.scene.timer.display_running_time())))
+        layout.label(text="Running Time: {.2f} seconds" .format(format_time(timer.display_running_time())))
 
         # Buttons
         layout.operator("timer.start", text="Start", icon='PLAY')

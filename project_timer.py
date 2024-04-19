f25a9413ce1c63ed7b011c4e6babc73d17919aea
incinemator <giannakzorz@gmail.com>
2024-04-19 14:41:50 +0200

    Update

--- project_timer.py
+++ project_timer.py
@@ -37,9 +37,15 @@ class TimerPropertyGroup(bpy.types.PropertyGroup):
         elif self.is_paused is not True and self.paused_time is 0:
             self.elapsed_time = time.time() - self.start_time
             self.start_time = 0
-        else:
+            self.paused_time = 0
+        elif self.is_paused and self.paused_time is 0:
             self.elapsed_time = self.paused_time
             self.start_time = 0
+            self.paused_time = 0
+        elif self.is_paused and self.paused_time is not 0:
+            self.elapsed_time = time.time() - self.paused_time
+            self.paused_time = 0
+
 
     # Pause the timer
     def pause(self):
